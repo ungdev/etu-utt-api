39d40a5b8fb6591f9d76f08c232dbcdc43e985f4
Thomas Ritaine <thomas.ritaine@outlook.com>
2023-04-23 17:22:22 -1000

    feat: :sparkles: Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('EtuUTT - API')
+    .setDescription(
+      "The API that allows to interact with the database of the student site to see, edit and delete some data according to the user's access.",
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
