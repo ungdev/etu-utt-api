0260d6699637a28f9adec370ee05916ee9e9597d
Teddy Roncin <teddy.roncin@proton.me>
2023-12-07 00:25:18 +0100

    deps: updated dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProfileModule } from './profile/profile.module';
-import { UsersModule } from "./users/users.module";
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
