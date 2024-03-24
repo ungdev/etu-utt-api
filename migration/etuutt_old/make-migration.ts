c719f78bf8317630a8478c782f6ae4d07518814a
Teddy Roncin <teddy.roncin@proton.me>
2024-03-24 02:54:40 +0100

    feat: started adding migration from old database to the new one

---
+++ migration/etuutt_old/make-migration.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+import { createConnection } from 'mysql';
+import { migrateUsers } from './modules/user';
+import { cleanDb } from "../../test/utils/test_utils";
+import { migrateUEs } from "./modules/ue";
+import { createCreditCategories } from "./modules/creditCategory";
+
+const prisma = new PrismaClient();
+
+const connection = createConnection({
+  host: 'localhost',
+  user: 'dev',
+  password: 'dev',
+  database: 'etuutt_old',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database');
+    return;
+  }
+  console.log('Connected to database');
+  main().then(() => {
+    prisma.$disconnect();
+    connection.end();
+  });
+});
+
+export type QueryFunction = (query: string) => Promise<any[]>;
+
+const query = (async (query: string) => {
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, res) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}) as QueryFunction;
+
+async function main() {
+  await cleanDb(prisma);
+  await createCreditCategories(prisma);
+  const ues = await migrateUEs(query, prisma);
+  await migrateUsers(query, prisma);
+}
---
+++ migration/etuutt_old/modules/user.ts
@@ -0,0 +1,89 @@
+import { PrismaClient, UserRole } from '@prisma/client';
+import { QueryFunction } from '../make-migration';
+import { RawUser } from "../../../src/prisma/types";
+
+export async function migrateUsers(query: QueryFunction, prisma: PrismaClient) {
+  const users = await query('SELECT * FROM etu_users LIMIT 10');
+  const promises: Array<Promise<RawUser>> = [];
+  for (const user of users) {
+    promises.push(
+      prisma.user.create({
+        data: {
+          login: user.login,
+          hash: '', // it is not possible to migrate the password, it's not possible to hash a password to an empty string
+          studentId: user.studentId,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          role: user.isStudent ? UserRole.STUDENT : UserRole.EMPLOYEE,
+          mailsPhones: {
+            create: {
+              mailUTT: user.mail,
+              mailPersonal: user.personnalMail,
+            },
+          },
+          infos: {
+            create: {
+              birthday: user.birthday,
+              nickname: user.surnom,
+              passions: user.passions,
+              website: user.website,
+              sex: user.sex,
+              avatar: user.avatar,
+              nationality: user.nationality,
+            },
+          },
+          socialNetwork: {
+            create: {
+              facebook: user.facebook,
+              twitter: user.twitter,
+              linkedin: user.linkedin,
+              discord: user.discordTag,
+              wantDiscordUTT: user.wantsJoinUTTDiscord,
+            },
+          },
+          privacy: {
+            create: {
+              mailUTT: true,
+              mailPersonal: user.personnalMail,
+              phoneNumber: user.phoneNumberPrivacy,
+              birthday: user.birthdayPrivacy,
+              birthdayDisplayOnlyAge: user.birthdayDisplayOnlyAge,
+              sex: user.sexPrivacy,
+              nationality: user.nationalityPrivacy,
+              discord: user.discordTagPrivacy,
+              address: !user.countryPrivacy
+                ? 'ALL_PRIVATE'
+                : !user.cityPrivacy
+                ? 'CITY_PRIVATE'
+                : !user.addressPrivacy
+                ? 'ADDRESS_PRIVATE'
+                : 'ALL_PUBLIC',
+            },
+          },
+          rgpd: {
+            create: {
+              isKeepingAccount: user.isKeepingAccount,
+              isDeletingEverything: user.isDeletingEverything,
+            },
+          },
+          preference: {
+            create: {
+              wantDaymail: user.daymail,
+              language: user.language,
+              wantDayNotif: false,
+            },
+          },
+          addresses: {
+            create: {
+              city: user.city,
+              country: user.country,
+              postalCode: user.postalCode,
+              street: user.address,
+            },
+          },
+        },
+      }),
+    );
+  }
+  return Promise.all(promises);
+}
