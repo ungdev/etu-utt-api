bf416da504dd542fa50a12592fbb8884bc115c71
Teddy Roncin <teddy.roncin@proton.me>
2024-03-24 19:02:18 +0100

    fix: removed commented code that is no longer useful

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,13 +13,6 @@ import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
-    /*ConfigModule.forRoot({
-      isGlobal: true,
-      // Ok, for some reason it still loads the normal .env.dev file.
-      // I tried to remove the ternary to make it always load the .env.dev.test file.
-      // It loads the .env.dev.test file properly, but overrides it with the normal .env.dev file
-      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env.dev',
-    }),*/
     ConfigModule,
     PrismaModule,
     AuthModule,
