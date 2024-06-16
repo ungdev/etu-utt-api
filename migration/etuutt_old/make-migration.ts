b28956e03ff19309f9f4c82c2d66c1ecd6a241bf
Teddy Roncin <teddy.roncin@proton.me>
2024-06-16 20:17:45 +0200

    feat: backup if a make something bad

--- migration/etuutt_old/make-migration.ts
+++ migration/etuutt_old/make-migration.ts
@@ -4,10 +4,10 @@ import { cleanDb } from '../../test/utils/test_utils';
 import { migrateUEs } from './modules/ue';
 import { createCreditCategories } from './modules/creditCategory';
 import { createSemesters } from './modules/semester';
-import { migrateUeComments } from './modules/ueComment';
+import {migrateUeComments, softMigrateUeComments} from './modules/ueComment';
 import { createBranches } from './modules/branch';
 
-const prisma = new PrismaClient();
+let prisma = new PrismaClient();
 console.log('Connected to new database');
 
 const res = /^mysql:\/\/(.*):(.*)@(.*):(.*)\/(.*)$/.exec(process.env.OLD_DATABASE_URL);
@@ -42,11 +42,47 @@ const query = (async (query: string) => {
 }) as QueryFunction;
 
 async function main() {
-  await cleanDb(prisma);
-  await createCreditCategories(prisma);
-  const semesters = await createSemesters(prisma);
-  await migrateUEs(query, prisma);
-  //await migrateUsers(query, prisma, ues, semesters);
-  await migrateUeComments(query, prisma, semesters);
-  await createBranches(prisma);
+  const argv = process.argv.slice(2);
+  if (argv[0] === '--drop-all') {
+    await cleanDb(prisma);
+    await createCreditCategories(prisma);
+    const semesters = await createSemesters(prisma);
+    await migrateUEs(query, prisma);
+    //await migrateUsers(query, prisma, ues, semesters);
+    await migrateUeComments(query, prisma, semesters);
+    await createBranches(prisma);
+  } else if (argv.length === 0) {
+    extendPrismaToSoftMigrate();
+    migrate();
+    const from = new Date(Date.parse(argv[1]));
+    if (isNaN(from.getTime())) {
+      console.error('Invalid timestamp');
+      return;
+    }
+    const semesters = await prisma.semester.findMany();
+    await softMigrateUeComments(query, prisma, semesters);
+  } else {
+    console.error('Invalid arguments. Usage: make-migration.ts [--drop-all]');
+  }
+}
+
+function extendPrismaToSoftMigrate() {
+  const originalPrisma = prisma;
+  prisma = prisma.$extends({
+    model: {
+      semester: {
+        async create({ code, start, end }: { code: string; start: Date; end: Date }) {
+          const semester = originalPrisma.semester.findFirst({ where: { code: this.code } });
+          if (!semester) {
+            return originalPrisma.semester.create({ data: this });
+          } else {
+            return semester;
+        }
+      }
+    }
+  })
+}
+
+function migrate() {
+
 }
--- migration/etuutt_old/modules/ueComment.ts
+++ migration/etuutt_old/modules/ueComment.ts
@@ -1,6 +1,6 @@
 import { QueryFunction } from '../make-migration';
 import { PrismaClient } from '@prisma/client';
-import { RawSemester } from '../../../src/prisma/types';
+import {RawSemester, RawUEComment} from '../../../src/prisma/types';
 
 export async function migrateUeComments(query: QueryFunction, prisma: PrismaClient, semesters: RawSemester[]) {
   const comments = await query(
@@ -8,7 +8,7 @@ export async function migrateUeComments(query: QueryFunction, prisma: PrismaClie
       'SELECT c.body, c.createdAt, c.updatedAt, c.isValide, u.code ' +
       'FROM etu_uvs_comments c ' +
       'INNER JOIN etu_uvs u ON c.uv_id = u.id ' +
-      'WHERE c.deletedAt IS NULL AND u.isOld = 0',
+      `WHERE c.deletedAt IS NULL AND u.isOld = 0`,
   );
   let createdCommentsCount = 0;
   for (const comment of comments) {
@@ -42,4 +42,71 @@ export async function migrateUeComments(query: QueryFunction, prisma: PrismaClie
   }
 
   console.log(`Migrated ${createdCommentsCount} comments`);
+}
+
+export async function softMigrateUeComments(query: QueryFunction, prisma: PrismaClient, semesters: RawSemester[]) {
+  const comments = await query(
+    '' +
+    'SELECT c.body, c.createdAt, c.updatedAt, c.isValide, u.code ' +
+    'FROM etu_uvs_comments c ' +
+    'INNER JOIN etu_uvs u ON c.uv_id = u.id ' +
+    `WHERE c.deletedAt IS NULL AND u.isOld = 0`,
+  );
+  let createdCommentCount = 0;
+  let updatedCommentsCount = 0;
+  const promises: Promise<RawUEComment>[] = [];
+  for (const comment of comments) {
+    const semester = semesters.find(
+      (semester) => semester.start <= comment.createdAt && semester.end >= comment.createdAt,
+    );
+    if (!semester) {
+      console.error(`Could not find semester for date ${comment.createdAt}. Comment is not going to be migrated.`);
+      continue;
+    }
+    promises.push(prisma.uEComment
+      .findFirst({
+        where: {
+          ue: {
+            code: comment.code,
+          },
+          createdAt: comment.createdAt,
+        },
+      })
+      .then((foundComment) => {
+        if (foundComment) {
+          return prisma.uEComment.update({
+            where: {
+              id: foundComment.id,
+            },
+            data: {
+              updatedAt: comment.updatedAt,
+              validatedAt: comment.isValide ? comment.updatedAt : null,
+              body: comment.body,
+            },
+          });
+        } else {
+          return prisma.uEComment.create({
+            data: {
+              body: comment.body,
+              isAnonymous: true,
+              createdAt: comment.createdAt,
+              updatedAt: comment.updatedAt,
+              validatedAt: comment.isValide ? comment.updatedAt : null,
+              ue: {
+                connect: {
+                  code: comment.code,
+                },
+              },
+              semester: {
+                connect: {
+                  code: semester.code,
+                },
+              },
+            },
+          });
+      }
+    }));
+  }
+  await Promise.all(promises);
+  console.log(`Migrated ${createdCommentCount} comments and soft-migrated ${updatedCommentsCount} comments`);
 }
