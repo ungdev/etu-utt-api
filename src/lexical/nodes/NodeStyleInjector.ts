c0a0c529787fd8f80fc0da60565ccd18f2c121ba
AlbanSdl <alban.delavoreille@free.fr>
2025-10-22 12:50:03 +0200

    fix: handle checklists and table header alignment

--- src/lexical/nodes/NodeStyleInjector.ts
+++ src/lexical/nodes/NodeStyleInjector.ts
@@ -1,13 +1,27 @@
+import { TableCellNode } from '@lexical/table';
 import { LexicalNode } from 'lexical';
 import { CustomStyles } from '.';
 
-function applyStylesToElement(element: HTMLElement, index: number) {
-  element.classList.forEach(
-    (cName) =>
-      cName in CustomStyles &&
-      (cName !== 'editor-table-row' || !(index % 2)) &&
-      Object.assign(cName === 'editor-image' ? element.querySelector('img').style : element.style, CustomStyles[cName]),
-  );
+function applyStylesToElement(element: HTMLElement, index: number, node?: LexicalNode) {
+  element.classList.forEach((cName) => {
+    if (!(cName in CustomStyles)) return;
+    if (cName === 'editor-list-item-checked' || cName === 'editor-list-item-unchecked')
+      Object.assign(element.style, CustomStyles['editor-list-item-check-base']);
+    else if (cName !== 'editor-table-row' || !(index % 2))
+      Object.assign(cName === 'editor-image' ? element.querySelector('img').style : element.style, CustomStyles[cName]);
+    if (cName === 'editor-table-cell-header' && node instanceof TableCellNode)
+      element.style.textAlign = node.__verticalAlign;
+    if (cName === 'editor-list-item-checked' || cName === 'editor-list-item-unchecked') {
+      const prependedElement = document.createElement('div');
+      Object.assign(prependedElement.style, CustomStyles[cName]);
+      if (cName === 'editor-list-item-checked') {
+        const iconElement = document.createElement('div');
+        Object.assign(iconElement.style, CustomStyles['editor-list-item-checked-icon']);
+        prependedElement.prepend(iconElement);
+      }
+      element.prepend(prependedElement);
+    }
+  });
   if (!element.className.includes('editor-ordered-list') && !element.className.includes('editor-unordered-list'))
     element.className = '';
 }
@@ -23,7 +37,7 @@ export function patchNodeExportDOM<T extends LexicalNode[]>(
     result.after = (element: HTMLElement | null) => {
       let updatedElement = element;
       if (originalAfterFunction) updatedElement = originalAfterFunction(element);
-      applyStylesToElement(updatedElement, this.getIndexWithinParent());
+      applyStylesToElement(updatedElement, this.getIndexWithinParent(), this);
       updatedElement
         .querySelectorAll('[class]')
         .forEach((element: HTMLElement) =>
