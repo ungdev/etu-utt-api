726182c90d0b47594509108786821080fef289e2
AlbanSdl <alban.delavoreille@free.fr>
2025-10-23 21:47:31 +0200

    docs: pimp docs with new theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'EtuUTT'
-copyright = '2024, UTT Net Group'
+copyright = '2025, UTT Net Group'
 author = 'UTT Net Group'
 
 
@@ -52,7 +52,7 @@ exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'shibuya'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
