7f3e5d576a392de44daad2a70680538de184a383
AlbanSdl <alban.delavoreille@free.fr>
2025-12-05 19:19:39 +0100

    fix: update prisma and downgrade ldapts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,10 +3,11 @@ import { join } from 'node:path';
 import { defineConfig } from 'prisma/config';
 
 const isTestEnv = process.env.NODE_ENV === 'test';
+// Production environnent variables should already be set in the container and are not read from file
 const envFile = isTestEnv ? '.env.test' : '.env.dev';
 let databaseURL: string | undefined;
 try {
-  readFileSync(join(process.cwd(), envFile), 'utf-8')
+  databaseURL = readFileSync(join(process.cwd(), envFile), 'utf-8')
     .split('\n')
     .find((line) => line.includes('DATABASE_URL='))
     ?.replace(/DATABASE_URL="?(\S+?)"?$/, '$1');
@@ -17,7 +18,6 @@ export default defineConfig({
   migrations: {
     path: join('prisma', 'migrations'),
   },
-  engine: 'classic',
   datasource: {
     url: process.env.DATABASE_URL || (process.env.DATABASE_URL = databaseURL),
   },
